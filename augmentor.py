ccbd0e8745a6aa1a81cb3f1bd07689e093499ccc
Michael Mendoza <m.mendoza711@gmail.com>
2018-03-30 17:37:29 -0600

    Refactor

--- augmentor.py
+++ augmentor.py
@@ -1,36 +1,25 @@
 
 import math
 import numpy as np
+from tqdm import trange, tqdm
 
+from options import AugmentOptions
 from loader import Loader
 from transform import Transform
 from sample import Sample
 
-class AugmentOptions:
-    def __init__(self):
-        ''' Probabilty of transformation'''
-        self.pScale = 0.5
-        self.pRotate = 0.5
-        self.pTranslate = 0.5
-
-        ''' Min/Max values for transformation '''
-        self.scale = (0.8, 1.2)
-        self.rotate = (0.0, math.pi)
-        self.tx = (-0.2, 0.2)
-        self.ty = (-0.2, 0.2)
-
-options = AugmentOptions()
-
 class Augmentor:
     def __init__(self):
-        self.options = options  
+        self.options = AugmentOptions() 
 
     def run(self, filepath, count):
         
         image = Loader.load('smile.jpg')        
         imgs = []
-        for _ in range(count):
-            print(_)
+
+        for _ in tqdm(range(count)):
+            #print(_)
+            options = self.options 
             scale = np.random.rand() * (options.scale[1] - options.scale[0]) + options.scale[0]
             rotate = np.random.rand() * (options.rotate[1] - options.rotate[0]) + options.rotate[0]
             tx = np.random.rand() * (options.tx[1] - options.tx[0]) + options.tx[0]
---
+++ options.py
@@ -0,0 +1,15 @@
+
+import math
+
+class AugmentOptions:
+    def __init__(self):
+        ''' Probabilty of transformation'''
+        self.pScale = 0.5
+        self.pRotate = 0.5
+        self.pTranslate = 0.5
+
+        ''' Min/Max values for transformation '''
+        self.scale = (0.8, 1.2)
+        self.rotate = (0.0, math.pi)
+        self.tx = (-0.2, 0.2)
+        self.ty = (-0.2, 0.2)
